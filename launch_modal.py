2a4d0007dd924348630872810643fed295c31059
Stephen Ren <stephrenny23@gmail.com>
2024-01-03 19:53:06 -0800

    Updated refresh behavior

--- launch_modal.py
+++ launch_modal.py
@@ -34,9 +34,9 @@ image = (
     .run_commands(install_commands)
     .pip_install(["Pillow", "boto3"])
     .run_function(download_models)
-    .run_commands("pwd", force_build=True) # Dev purposes
-    .workdir('facefusion-custom')
-    .run_commands("git pull")
+    # .run_commands("pwd", force_build=True) # Dev purposes
+    # .workdir('facefusion-custom')
+    # .run_commands("git pull")
 )
 
 vol = Volume.persisted("alias-sources")
